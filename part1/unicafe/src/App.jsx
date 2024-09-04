5a1d5746fb9dd4a7a2f88c4de10b8c6ee831e511
Topias Pesonen <topias.pesonen@gmail.com>
2024-09-04 15:59:51 +0300

    output as a table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,28 +10,49 @@ const Button = (props) => (
   </button>
 )
 
-const Statistics = ({good, neutral, bad}) =>(
-  <div>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-  </div>
-)
+const Statistics = ({good, neutral, bad, allReviews}) =>{
+
+  const sum = allReviews.reduce((a, b) => a + b, 0)
+  const all = allReviews.length
+  const avg = (sum / all) || 0
+  const pos = (good / all * 100) || 0 
+
+  console.log({good}) 
+  console.log({pos})
+  return (
+    <table>
+      <tbody>
+        <tr><td>good</td><td>{good}</td></tr>
+        <tr><td>neutral</td><td>{neutral}</td></tr>
+        <tr><td>bad</td><td>{bad}</td></tr>
+        <tr><td>all</td><td>{all}</td></tr>
+        <tr><td>average</td><td>{avg.toFixed(1)}</td></tr>
+        <tr><td>positive</td><td>{pos.toFixed(1)}%</td></tr>
+      </tbody>
+    </table>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allReviews, setAll] = useState([])
+  console.log(allReviews)
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setAll(allReviews.concat(1))
   }
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(allReviews.concat(0))
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(allReviews.concat(-1))
   }
 
 
@@ -42,7 +63,7 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text="neutral" />
       <Button handleClick={handleBadClick} text="bad" />
       <Header header="statistics"/>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} allReviews={allReviews}/>
     </div>
   )
 }
