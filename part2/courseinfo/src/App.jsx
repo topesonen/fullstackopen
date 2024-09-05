bf1066cfe3300a0e736fc8d0a83c9f3dc43494bd
Topias Pesonen <topias.pesonen@gmail.com>
2024-09-05 14:44:14 +0300

    2.4 done

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,19 +1,24 @@
 const Course = ({ course }) => { 
   console.log(course)
+
+  const sum = course.parts.reduce((a, b) => a + b.exercises, 0)
+  console.log(sum);
+  
   return (
     <div>
       <h1>{course.name}</h1>
         {course.parts.map(part => 
           <p key={part.id}>
             {part.name} {part.exercises}
-          </p>
+          </p>  
         )}
+      <b>total of {sum} exercises</b>
     </div>
   )
 }
 
 const App = () => {
-  const course = {
+  const courses = [{
     id: 1,
     name: 'Half Stack application development',
     parts: [
@@ -31,13 +36,42 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  },
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
       }
     ]
   }
+]
 
-  
+return (
+  <div>
+    {courses.map(course =>(
+      <Course key={course.id} course={course} />
+    ))}
+  </div>
+)
 
-  return <Course course={course} />
+
+
 }
 
 export default App
