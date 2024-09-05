2c8f5f25aa20ecfa302f3960db0269787e3f89e0
Topias Pesonen <topias.pesonen@gmail.com>
2024-09-05 20:13:23 +0300

    name exists phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,54 @@
 import { useState } from 'react'
 
+const Person = ({person}) => {
+  return (
+    <div>{person.name}</div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', id:1}
   ]) 
   const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName, 
+      id: persons.length + 1
+    }
+
+    if (persons.some(person => person.name === newName)) {
+      console.log("already in phonebook", {newName})
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>(
+        <Person key={person.id} person={person} />
+    ))}
     </div>
   )
 }
